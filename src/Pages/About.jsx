26c061fb86cd067a44ac3227c0d0c3354491d935
aydenapollo <mcdonaldshane33@gmail.com>
2022-12-31 18:16:33 -0600

    done..just a cosmetic website

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import '../Styles/About.css';
 function About() {
   return (
-    <div>
-      WORKING?
+    <div className='about'>
+      <div className='aboutTop' style = {{backgroundImage: `url(https://cdn.geekwire.com/wp-content/uploads/2015/05/pizza.jpg)`}}></div>
+      <div className='aboutBottom'>
+        <h1>ABOUT US</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptatibus adipisci ipsa perspiciatis laudantium veritatis, labore et est molestiae, tenetur sint voluptates. Harum voluptatum, sapiente consectetur dolor reiciendis veniam quos!</p>
+      </div>
     </div>
   )
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,23 @@
+
 import React from 'react';
 import '../Styles/Contact.css';
 
 function Contact() {
   return (
-    <div>
-      Working?
+    <div className='contact'>
+        <div className='leftSide' style = {{backgroundImage: `url(https://cdn.geekwire.com/wp-content/uploads/2015/05/pizza.jpg)`}}></div>
+        <div className='rightSide'>
+            <h1> Contact Us</h1>
+            <form id="contact-form" method="POST">
+                <label htmlFor="name">Full Name</label>
+                <input name="name" placeholder='Enter Full Name...' type="text" />
+                <label htmlFor="name">Email</label>
+                <input name="name" placeholder='Enter Your Email...' type="email" />
+                <label htmlFor="message">Message</label>
+                <textarea rows='6' placeholder='Enter Message...' name="message" required></textarea>
+                <button type='submit'>Send Message</button>
+            </form>
+        </div>
     </div>
   )
 }
